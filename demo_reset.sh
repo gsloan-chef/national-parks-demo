2cbfd0175736fbce3621d6536ca67406ea0957e6
gsloan-chef <swankgd@gmail.com>
2018-11-01 13:13:05 -0400

    fix plan.sh

--- demo_reset.sh
+++ demo_reset.sh
@@ -4,7 +4,7 @@ rm -rf src/main/webapp/index.html
 mv index-orig.html src/main/webapp/index.html
 
 sed -E -i "s:v[0-9]+\.[0-9]+\.[0-9]+:$1:" src/main/webapp/index.html
-sed -E -i "s:pkg_version=[0-9]+\.[0-9]+\.[0-9]+:$1:" habitat/plan.sh
+sed -E -i "s:pkg_version=[0-9]+\.[0-9]+\.[0-9]+:pkg_version=$1:" habitat/plan.sh
 sed -E -i "s:v[0-9]+\.[0-0]+\.[0-9]+:$2:" red-index.html
 
 git add .
