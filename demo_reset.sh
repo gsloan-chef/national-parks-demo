fc8fc0f1a1de012957ac9f818d757ea2ce47e3cd
gsloan-chef <swankgd@gmail.com>
2018-11-01 13:43:02 -0400

    demo reset

--- demo_reset.sh
+++ demo_reset.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  echo 
+else
+  echo "Invalid version"
+  exit
+fi
+
+if [[ $2 =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  echo 
+else
+  echo "Invalid version"
+  exit
+fi
+
 rm -rf src/main/webapp/index.html
 mv index-orig.html src/main/webapp/index.html
 
-sed -E -i "s:v[0-9]+\.[0-9]+\.[0-9]+:$1:" src/main/webapp/index.html
+sed -E -i "s:v[0-9]+\.[0-9]+\.[0-9]+:v$1:" src/main/webapp/index.html
 sed -E -i "s:pkg_version=[0-9]+\.[0-9]+\.[0-9]+:pkg_version=$1:" habitat/plan.sh
-sed -E -i "s:v[0-9]+\.[0-0]+\.[0-9]+:$2:" red-index.html
+sed -E -i "s:v[0-9]+\.[0-9]+\.[0-9]+:v$2:" red-index.html
 
-git add .
-git commit -m "Demo reset and rev"
-git push origin master
+#git add .
+#git commit -m "Demo reset and rev"
+#git push origin master
